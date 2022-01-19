e02e32dc208b3d1ef9cc445120d32fbbaa866205
Michal <micmey9@gmail.com>
2022-01-20 00:54:19 +0100

    added random city generator

--- main.cu
+++ main.cu
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct city{
+    int posX;
+    int posY;
+    bool visited;
+};
+
+city random_city(){
+    city rand_city{};
+    rand_city.posX = rand()%100;
+    rand_city.posY = rand()%100;
+    rand_city.visited = false;
+    return rand_city;
+}
+
+
+vector<city> generate_cities(unsigned short no_cities){
+    vector<city> cities(no_cities);
+    generate(cities.begin(), cities.end(), random_city);
+    return cities;
+}
+
+
 
 int main() {
-    std::cout << "Traveling salesman problem" << std::endl;
+    unsigned short n = 20;
+    auto cities = generate_cities(n);
+
     return 0;
 }
